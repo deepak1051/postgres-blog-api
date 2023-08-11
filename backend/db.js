c5679c62b8c02a983c8e321f2406df405029cd6b
deepak1051 <lightyagamigoku@gmail.com>
2023-08-11 13:27:38 +0530

    neon string

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,9 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  // connectionString: process.env.CONNECTION_STRING,
-  connectionString:
-    'postgres://deepak1051:MWmvn2FVl8Kq@ep-fancy-firefly-85782086.ap-southeast-1.aws.neon.tech/neondb',
+  connectionString: process.env.CONNECTION_STRING,
+  // connectionString:
+  //   'postgres://deepak1051:MWmvn2FVl8Kq@ep-fancy-firefly-85782086.ap-southeast-1.aws.neon.tech/neondb',
   ssl: {
     rejectUnauthorized: false,
   },
